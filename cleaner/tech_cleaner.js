437d73b4a703bcaf6a9dbdb505486922ce7d06f3
abhik <abhiksahu2003@gmail.com>
2025-05-22 02:15:56 +0530

    completed work on cleaners and scrappers

---
+++ cleaner/tech_cleaner.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+const webregex = /(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/g;
+
+const phoneregex = /^(\+91[\-\s]?)?[0]?(91)?[789]\d{9,12}$/g;
+const streetregex = /\d{1,3}\s+\w+\s+(road|street|lane|marg|nagar|sector|block)/gi;
+
+const raw_data = fs.readFileSync('../data/raw/tech.txt', 'utf8');
+
+const all_sentences = raw_data
+    .split('।')
+    .map(sentence => sentence.trim())
+    .filter(sentence => sentence.length > 0);
+
+const cleaned = [];
+const junk = [];
+
+for (const sentence of all_sentences) {
+    const changed = sentence.replace(webregex, 'website');
+
+    const isjunk = phoneregex.test(changed) || streetregex.test(changed);
+    if(isjunk){
+        junk.push(changed);
+    }
+    else{
+        cleaned.push(changed);
+    }
+}
+
+
+fs.writeFileSync('../data/cleaned/tech.txt', cleaned.join('।\n'));
+fs.writeFileSync('../data/junk/tech.txt', junk.join('।\n'));
---
+++ scrapper/opinion_scrapper.js
@@ -0,0 +1,41 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+const url = `https://www.bhaskar.com/opinion`
+
+async function scrape() {
+    try {
+        const { data } = await axios.get(url);
+        const scrape_data = cheerio.load(data);
+
+        const articles = [];
+
+        scrape_data('a').each((_, element) => {
+            const link = scrape_data(element).attr('href');
+            if (link && link.includes('/opinion/')) {
+                articles.push(`https://www.bhaskar.com${link}`);
+            }
+        });
+
+        const uniquearticles = [...new Set(articles)].slice(0, 10);
+
+        for (let i = 0; i < uniquearticles.length; i++) {
+            const { data:article_data } = await axios.get(uniquearticles[i]);
+            const scrape_article_data = cheerio.load(article_data);
+
+
+            const content = scrape_article_data('article p')
+                .map((_, element) => scrape_article_data(element).text().trim())
+                .get()
+                .join('\n');
+
+            fs.appendFileSync('../data/raw/opinion.txt', content + '\n\n');
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+scrape();
